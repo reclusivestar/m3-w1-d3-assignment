71d7fcd5a9370fbbd40248257ad641c96b399585
Kumar Tanmay <tanmay.kumar@viacomcbs.com>
2024-07-11 22:59:17 -0400

    added registration feature

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
+const path = require('path')
+const auth = require('http-auth')
 const express = require('express')
+const mongoose = require('mongoose');
 const router = express.Router();
 const { check, validationResult } = require('express-validator') 
+const Registration = mongoose.model('Registration');
+
+const basic = auth.basic({
+    file: path.join(__dirname, '../users.htpasswd'),
+})
 
 router.get('/', function(req, res) {
     res.render('form', { title: 'Registration form'});
 })
+
+router.get('/registrations', basic.check((req, res) => {
+    Registration.find()
+    .then((registrations) => {
+        res.render('index', { title: 'Listing registrations', registrations});
+    })
+    .catch(() => { res.send('Sorry! Something went wrong.');});
+}));
 
 router.post('/', 
 [
@@ -19,9 +35,15 @@ function(req, res) {
     console.log(req.body)
     const errors = validationResult(req);
     if (errors.isEmpty()) {
-        res.send('Thank you for your registration!');
+        const registration = new Registration(req.body);
+        registration.save()
+        .then(() => { res.send('Thank you for your registration!');})
+        .catch((err) => {
+            console.log(err);
+            res.send('Sorry! Something went wrong')
+        });
     } else {
-    res.render('form', { title: 'Registration form', errors: errors.array(), data: req.body});
+        res.render('form', { title: 'Registration form', errors: errors.array(), data: req.body});
     }
 })
 
--- start.js
+++ start.js
@@ -1,5 +1,20 @@
-const app = require('./app');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.DATABASE, {
+    useUnifiedTopology: true
+});
+
+mongoose.connection
+    .on('open', () => {
+        console.log('Mongoose connection open');
+    })
+    .on('error', (err) => {
+        console.log(`Connection error: ${err.message}`);
+    });
+
+require('./models/Registration');
+const app = require('./app');
 const server = app.listen(3000, function() {
     console.log(`Express is running on port ${server.address().port}`);
 });
